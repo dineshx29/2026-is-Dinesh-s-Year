c7a5608503648319c32a00421a7566fc2921c201
Dinesh <dinesh.b.offl@gmail.com>
2026-07-08 22:25:52 +0530

    feat(leetcode): solve Valid Anagram [Python]

---
+++ LeetCode/0242-Valid Anagram/0242-Valid Anagram - method 7.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s=(s.lower()).replace(" "."")
+        t=(t.lower()).replace(" "."")
+        count=[0]*26
+        for char in s:
+            count[ord(char)-ord('a')]+=1
+            
