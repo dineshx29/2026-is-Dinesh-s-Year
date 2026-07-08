5f776ef2740e3df56309acca24abc386b728f79a
Dinesh <dinesh.b.offl@gmail.com>
2026-07-08 20:29:19 +0530

    Delete LeetCode/0100-Two Sum directory

--- LeetCode/0100-Two Sum/0100-Two Sum.py
+++
@@ -1,8 +0,0 @@
-1class Solution:
-2    def twoSum(self, nums: List[int], target: int) -> List[int]:
-3        hashmap={}
-4        for i in range(len(nums)):
-5            complement=target-nums[i]
-6            if complement in hashmap:
-7                return [hashmap[complement],i]
-8            hashmap[nums[i]]=i
