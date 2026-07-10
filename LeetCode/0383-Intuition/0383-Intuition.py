60dd122a375c23dba8b0671fa5b07b11c8e568ec
Dinesh <dinesh.b.offl@gmail.com>
2026-07-10 23:15:14 +0530

    Delete LeetCode/0383-Intuition directory

--- LeetCode/0383-Intuition/0383-Intuition.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        count=Counter(magazine)
-        for c in ransomNote:
-            if count[c]==0:
-                return False
-            count[c]-=1
-        return True
